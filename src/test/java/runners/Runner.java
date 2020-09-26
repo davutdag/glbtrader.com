bfe552e8e37fa84894a9801ac53be01ad4d04a78
mustafaoz006 <moz006045@gmail.com>
2020-09-26 19:20:45 +0300

    third

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "stepdefinitions",
-        tags = "@test1",
+        tags = "@test",
         dryRun = false
 )
 
